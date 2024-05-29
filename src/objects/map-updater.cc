a4ee1211ebe3b3d66500f274c32f58688298e231
cuilamwang <cuilamwang@tencent.com>
2024-05-29 14:31:00 +0800

    fix：修复编译问题cherry-pick 807cf7d           https://flow.woa.com/workbench/detail/32040300042

--- src/objects/map-updater.cc
+++ src/objects/map-updater.cc
@@ -1033,10 +1033,10 @@ MapUpdater::State MapUpdater::ConstructNewMap() {
 
   bool had_any_enum_cache =
       split_map->instance_descriptors(isolate_)
-              ->enum_cache()
-              ->keys()
-              ->length() > 0 ||
-      old_descriptors_->enum_cache()->keys()->length() > 0;
+              .enum_cache()
+              .keys()
+              .length() > 0 ||
+      old_descriptors_.enum_cache().keys().length() > 0;
 
   // Deprecated part of the transition tree is no longer reachable, so replace
   // current instance descriptors in the "survived" part of the tree with
