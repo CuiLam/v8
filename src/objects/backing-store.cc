37dc3bbc2b72e7834b2bf082f7d51970ed9d8840
cuilamwang <cuilamwang@tencent.com>
2024-06-13 16:10:49 +0800

    chore: add test log

--- src/objects/backing-store.cc
+++ src/objects/backing-store.cc
@@ -362,7 +362,7 @@ std::unique_ptr<BackingStore> BackingStore::TryAllocateAndPartiallyCommitMemory(
           MemoryPressureLevel::kCritical, true);
     }
     TRACE_BS("cuilamTest: gc retry return false\n");
-    return false;
+    return true;
   };
 
   // 1,073,741,824
