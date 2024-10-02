88c0aabb7e422fa96b3ad5be12ae223d4c3c3af2
Victor Gomes <victorgomes@chromium.org>
2024-10-02 10:59:42 +0200

    cherry-pick 81155a8f3b20fbfc7e36c2419f5326f1d0ad7d75
    https://flow.woa.com/workbench/detail/32100900028
    
    [turbofan] Consider WasmStruct in InferHasInPrototypeChain
    
    Drive-by: add some CHECKs in not _clearly_ safe uses of AsJSObject
    to turn possible vulnerablities into crashes.
    
    Fixed: 367818758
    Change-Id: Ib0464658152ce87141fa137dc6562f17b84bb6be
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/5901846
    Reviewed-by: Nico Hartmann <nicohartmann@chromium.org>
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Commit-Queue: Nico Hartmann <nicohartmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#96386}

--- src/compiler/heap-refs.cc
+++ src/compiler/heap-refs.cc
@@ -1550,6 +1550,7 @@ HolderLookupResult FunctionTemplateInfoRef::LookupHolderOfExpectedType(
   if (!expected_receiver_type->IsTemplateFor(prototype.object()->map())) {
     return not_found;
   }
+  CHECK(prototype.IsJSObject());
   return HolderLookupResult(CallOptimization::kHolderFound,
                             prototype.AsJSObject());
 }
