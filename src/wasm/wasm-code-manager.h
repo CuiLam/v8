74285bf6a7d65501c89c1971c29a49aa93095c79
cuilamwang <cuilamwang@tencent.com>
2024-12-11 17:53:12 +0800

    fix build b4d7cc4d EstimateCurrentMemoryConsumption

--- src/wasm/wasm-code-manager.h
+++ src/wasm/wasm-code-manager.h
@@ -887,6 +887,8 @@ class V8_EXPORT_PRIVATE NativeModule final {
   // Get or create the NamesProvider. Requires {HasWireBytes()}.
   NamesProvider* GetNamesProvider();
 
+  size_t EstimateCurrentMemoryConsumption();
+
   uint32_t* tiering_budget_array() { return tiering_budgets_.get(); }
 
   Counters* counters() const { return code_allocator_.counters(); }
